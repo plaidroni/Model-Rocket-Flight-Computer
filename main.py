a158830faf5f09030bb7197c1f4ab6a1cf358b2d
dietb <nathanielcovington1001@gmail.com>
2026-04-25 16:11:29 -0700

    v1 of station code

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
 import tkinter as tk
-from tkinter import filedialog
 import utils
-import shutil
-import numpy as np
 import os
+from PIL import Image, ImageTk
+import json
 
-max_value = 1
-min_value = 0
+base_dir = os.path.join(os.getcwd())
+
+
+with open(os.path.join(base_dir, 'graph_settings.json'), 'r') as file:
+    settings = json.load(file)
+
 current_value = 0
+max_altitude = 10000
 
 def up_button():
     global current_value
@@ -22,36 +24,28 @@ def down_button():
     current_value = (current_value - 1) % len(Stacked_frames)
     Stacked_frames[current_value].tkraise()
 
-def button_press():
-    current_path = os.getcwd()
-    base_dir = os.path.join(current_path, "Rocket Launch Data sets")
-    folder_name_start = "Launch_Data_#"
-    folder_path = utils.create_folder(base_dir, folder_name_start)
-    file_path = open_file(folder_path)
-    if os.path.getsize(file_path) == 0:
-        print("File is empty")
-        return
-    else:
-        data = np.genfromtxt(file_path, delimiter=",", skip_header=2, names=True)
-        time = data["time"]
-        temp = data["temp"]
-        pressure = data["pressure"]
-        humidity = data["humidity"]
-    canvas = FigureCanvasTkAgg(utils.plot_tvt(folder_path, time, temp), master = TvA_frame)
-    canvas.draw()
-    canvas.get_tk_widget().pack(fill="both", expand=True)
-    #utils.plot_pvt(folder_path, time, pressure)
-    #utils.plot_hvt(folder_path, time, humidity)
+def open_btn():
+    global settings
+    txt_file = utils.open_file()
+    folder = utils.create_graphs(txt_file, settings)
+    load_graphs(folder)
 
 
-def open_file(folder_path):
-    file_path = filedialog.askopenfilename(title = "Select a Text File",
-    filetypes = (("Text files", "*.txt"),))
-    if file_path:
-        file_name.config(text=f"Selected: {file_path}")
-        file_path = shutil.move(file_path, folder_path)
-    return file_path
+def view_btn():
+    load_graphs(utils.open_folder())
 
+def load_graphs(folder):
+    labels = [wip, wip2, wip3]
+    names = ["graph1.png", "graph2.png", "graph3.png"]
+
+    images = []
+
+    for label, name in zip(labels, names):
+        img = Image.open(os.path.join(folder, name))
+        photo = ImageTk.PhotoImage(img)
+        label.config(image=photo)
+        label.image = photo
+        images.append(photo)
 
 
 #root setup
@@ -63,33 +57,54 @@ root.overrideredirect(True)
 root.configure(background='gray54')
 
 #topframe
-top_frame = tk.Frame(root, bg='gray26', height='40')
+top_frame = tk.Frame(root, bg='gray26', height='40', relief='solid', borderwidth = 1,)
 top_frame.pack_propagate(False)
 top_frame.pack(side="top", fill="x")
 #top frame widgets
 top_format = {"font": ("Terminal", 12), "bg": "gray26", "fg": "gray54", 'relief': 'flat', 'activebackground': 'red', 'borderwidth': '0'}
-title_label = tk.Label(top_frame, text='Time vs Altitude', **top_format)
+title_label = tk.Label(top_frame, text='Rocket Launch Data Visualizer', **top_format)
 exit_btn = tk.Button(top_frame, text = 'X', command = root.destroy, **top_format, width=3)
 #widget placement
 title_label.pack(side="left")
 exit_btn.pack(side='right', anchor='e')
 
 #btm_frame
-btm_frame = tk.Frame(root, bg='gray26', height='200')
+btm_frame = tk.Frame(root, bg='gray26', height='200', relief='solid', borderwidth = 1)
 btm_frame.pack_propagate(False)
 btm_frame.pack(side="bottom", fill="x")
 #btm_widgets
 btm_format = {"font": ("Terminal", 12), "bg": "gray26", "fg": "gray54", 'relief': 'flat', 'borderwidth': '0'}
 switch_left_btn = tk.Button(btm_frame, command = down_button, text = '<', **btm_format, width=3)
 switch_right_btn = tk.Button(btm_frame, command = up_button, text = '>', **btm_format, width=3)
-select_file_btn = tk.Button(btm_frame, text = 'Select File', **btm_format, command = button_press)
+
+select_file_btn = tk.Button(btm_frame, text = 'Upload Data', **btm_format, command = open_btn)
+select_folder_btn = tk.Button(btm_frame, text = 'View Data', **btm_format, command = view_btn)
 file_name = tk.Label(btm_frame, **btm_format)
 #widget placement
 switch_left_btn.pack(side='left', anchor='n')
 switch_right_btn.pack(side='left', anchor='n')
+
 select_file_btn.pack(side='right', anchor='n')
+select_folder_btn.pack(side='right', anchor='n')
 file_name.pack(side='right', anchor='n')
 
+#side frame
+side_frame = tk.Frame(root, bg='gray26', width='200',)
+side_frame.pack_propagate(False)
+side_frame.pack(side="left", fill="y")
+#side widgets
+side_format = {"font": ("Terminal", 12), "bg": "gray26", "fg": "gray54", 'relief': 'flat', 'borderwidth': '0'}
+max_alt_label = tk.Label(side_frame, **side_format, text = f'Apogee={max_altitude} m')
+dlable1 = tk.Label(side_frame, **side_format, text = f'data1={max_altitude} m')
+dlable2 =tk.Label(side_frame, **side_format, text = f'data2={max_altitude} m')
+dlable3 =tk.Label(side_frame, **side_format, text = f'data3={max_altitude} m')
+dlable4 =tk.Label(side_frame, **side_format, text = f'data4={max_altitude} m')
+#widget placement
+max_alt_label.pack(side="top")
+dlable1.pack(side="top")
+dlable2.pack(side="top")
+dlable3.pack(side="top")
+dlable4.pack(side="top")
 
 #center frame
 cen_frame = tk.Frame(root, bg='gray1')
@@ -105,9 +120,14 @@ TvAc_frame.place(relwidth=1, relheight=1)
 wip2 = tk.Label(TvAc_frame, text='WIP2', **top_format)
 wip2.pack()
 
+TvAcc_frame = tk.Frame(cen_frame, bg='gray30')
+TvAcc_frame.place(relwidth=1, relheight=1)
+wip3 = tk.Label(TvAcc_frame, text='WIP3', **top_format)
+wip3.pack()
 
 
-Stacked_frames = [TvA_frame, TvAc_frame]
+
+Stacked_frames = [TvA_frame, TvAc_frame, TvAcc_frame]
 Stacked_frames[current_value].tkraise()
 
 root.mainloop()
--- utils.py
+++ utils.py
@@ -1,62 +1,99 @@
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
 #!/usr/bin/env python3
+from matplotlib import pyplot as plt
+from tkinter import filedialog
+import shutil
+import numpy as np
 import os
+import math
+
+base_dir = os.path.join(os.getcwd(), "Rocket Launch Data sets")
 
 
 
-
-def create_folder(base_dir, folder_name_start):
-
+def create_folder():
+    global base_dir
     os.makedirs(base_dir,exist_ok=True)
     existing_folders = [
         name for name in os.listdir(base_dir)
-        if os.path.isdir(os.path.join(base_dir, name)) and name.startswith(folder_name_start)
+        if os.path.isdir(os.path.join(base_dir, name)) and name.startswith('Launch_Data_#')
     ]
     next_number = len(existing_folders)+ 1
-    folder_name = f"{folder_name_start}{next_number:03d}"
+    folder_name = f"Launch_Data_#{next_number:03d}"
     folder_path = os.path.join(base_dir,folder_name)
     os.makedirs(folder_path)
     return folder_path
 
-def plot_tvt(folder_path, time, temp):
-    tvt_plot_file = os.path.join(folder_path, "temperature_plot.png")
-    fig, ax = plt.subplots()
-    ax.plot(time, temp, label="Temperature", color='red')
-    ax.set_title("Temperature vs Time")
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Temp")
-    ax.legend()
-    ax.grid()
 
-    plt.savefig(tvt_plot_file)
-    plt.show()
+def open_file():
+    path = filedialog.askopenfilename(title="Select a Text File", filetypes=(("Text files", "*.txt"),))
+    return path
+
+def open_folder():
+    global base_dir
+    path = filedialog.askdirectory(initialdir=base_dir, title="Select a Folder")
+    return path
+
+def create_graphs(txt_file, settings):
+    folder_path = create_folder()
+    txt_file = shutil.move(txt_file, folder_path)
+    data = np.genfromtxt(txt_file, delimiter=",", skip_header=2, names=True)
+    #name data
+    time = data["time"]
+    temp = data["temp"]
+    pressure = data["pressure"]
+    altitude = calc(pressure)
+    air_density = calc(temp)
+    #create data based on settings
+    save_fig(plot_graph(time, temp, settings, "time", "temp", "time vs temp"), folder_path, "graph1.png")
+    save_fig(plot_graph(time, altitude, settings, "altitude", "time", "altitude vs time"), folder_path, "graph2.png")
+    save_fig(plot_graph(altitude, air_density, settings, altitude, air_density, "altitude vs air_density"), folder_path, "graph3.png")
+    return folder_path
+
+def plot_graph(x, y, settings, ylabel, xlabel, label):
+    plt.style.use(settings["style"])
+    fig, ax = plt.subplots()
+
+    if settings["graph_type"] == "line":
+        ax.plot(x, y, color=settings["color"], label = label)
+
+    elif settings["graph_type"] == "scatter":
+        ax.scatter(x, y, color=settings["color"], label = label)
+
+    elif settings["graph_type"] == "bar":
+        ax.bar(x, y, color=settings["color"], label = label)
+
+    ax.set_title(settings["title"])
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if settings["legend"]:
+        ax.legend()
+
+    if settings["grid"]:
+        ax.grid()
+
     return fig
 
-def plot_pvt(folder_path, time, pressure):
-    pvt_plot_file = os.path.join(folder_path, "pressure_plot.png")
-    plt.figure()
-    plt.plot(time, pressure, label="pressure (°C)", color='red')
-    plt.xlabel("Time")
-    plt.ylabel("pressure")
-    plt.title("pressure vs Time")
-    plt.legend()
-    plt.grid()
+def save_fig(fig, folder_path, file_name):
+    path = os.path.join(folder_path, file_name)
+    fig.savefig(path)
+    plt.close(fig)
+    return path
 
-    plt.savefig(pvt_plot_file)
-    plt.show()
-    return pvt_plot_file
+def calc(pressure):
+    altitude = 44330 * (1 - pow(pressure / 101.325, 1 / 5.255))
+    return altitude
 
-def plot_hvt(folder_path, time, humidity):
-    hvt_plot_file = os.path.join(folder_path, "humidity_plot.png")
-    plt.figure()
-    plt.plot(time, humidity, label="humidity (°C)", color='red')
-    plt.xlabel("Time")
-    plt.ylabel("humidity")
-    plt.title("humidity vs Time")
-    plt.legend()
-    plt.grid()
+def calc2(temp, humidity, pressure):
+    a = 0.61078
+    b = 17.27
+    c = 237.3
+    svp = a*math.exp((b*temp)/(temp+c))
+    p_v = (humidity/100)*svp
+    p_d = pressure
+    sgca = 287.05
+    sgcv = 461.495
 
-    plt.savefig(hvt_plot_file)
-    plt.show()
-    return hvt_plot_file
+    temp_k=temp+273.15
+    air_density = (p_d/sgca*temp_k)+(p_v/sgcv*temp_k)
+    print("air density",air_density)
+    return air_density
